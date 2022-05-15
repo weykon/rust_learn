1dc2ecd5b7548a8264112c23e5e26317f81c804f
weykonkong <weykonkong@gmail.com>
2022-05-15 18:39:12 +0800

    lesson_1: input random number

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use ferris_says::say; // from the previous step
-use std::io::{stdout, BufWriter};
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    println!("Guess the number!");
+
+    println!("Please input your guess");
+
+    let mut guess = String::new();
+
+    std::io::stdin().read_line(&mut guess).expect("Failed to read line");
+
+    println!("You guessed: {}",guess);
 }
