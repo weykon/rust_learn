6097078510e8b7a418b5d23202a9870eddfa6a3b
weykonkong <weykonkong@gmail.com>
2022-05-15 19:05:01 +0800

    lesson_2: match & type change

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,30 @@
+use std::cmp::Ordering;
+use rand::{thread_rng, Rng};
+use std::io;
 
 fn main() {
     println!("Guess the number!");
+    let mut rng = thread_rng();
 
-    println!("Please input your guess");
+    let secret_number: u32 = rng.gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
+
+    println!("Please input your guess.");
 
     let mut guess = String::new();
 
-    std::io::stdin().read_line(&mut guess).expect("Failed to read line");
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
 
-    println!("You guessed: {}",guess);
+    let guess:u32 = guess.trim().parse().expect("Please type a number!");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
+
+    println!("You guessed: {}", guess);
 }
