aab1d93e0be72fa97484da594dd2f4214d979380
weykon <635883092@qq.com>
2022-05-17 01:50:28 +0800

    lesson_8_2: loop循环中返回值

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,12 @@
 fn main() {
-    let condition = true;
+    let mut counter = 0;
 
-    let number = if condition { 5 } else { 6 };
+    let result = loop{
+        counter += 1;
+        if counter == 10 {
+            break counter * 2;
+        }
+    };
 
-    println!("The value of number is : {}", number);
+    println!("The result is {}", result);
 }
