f8175763c0308c6e6313872e0f4628f8a9470c7c
weykon <635883092@qq.com>
2022-05-16 11:15:23 +0800

    lesson_6: 数组索引一下

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,21 @@
-use rand::{thread_rng, Rng};
-use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Guess the number!");
-    let mut rng = thread_rng();
+    let a = [1, 2, 3, 4, 5];
 
-    let secret_number: u32 = rng.gen_range(1, 101);
+    println!("Please enter an array index.");
 
-    println!("The secret number is: {}", secret_number);
+    let mut index = String::new();
 
-    loop {
-        println!("Please input your guess.");
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
 
-        let mut guess = String::new(); // 这条如果放在loop外面就不能正常运行了
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
 
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guess: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-    }
+    let element = a[index];
+    println!("The value of element at index {} is {}", index, element);
 }
