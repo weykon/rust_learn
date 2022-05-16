15a162902277b0ce161e86525f444865985ca4cd
weykon <635883092@qq.com>
2022-05-17 01:57:33 +0800

    lesson_8_3: for

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 fn main() {
-    let mut counter = 0;
+    
+    let a = [10,20,30,40,50];
 
-    let result = loop{
-        counter += 1;
-        if counter == 10 {
-            break counter * 2;
-        }
-    };
+    for element in a.iter() {
+        println!("The result is {}", element);
+    }
 
-    println!("The result is {}", result);
+    for element in (1..4).rev(){
+        println!("{}!",element)
+    }
 }
