fc78a565e1c9f12bcad415c985664d177562f51e
weykon <635883092@qq.com>
2022-05-17 01:33:58 +0800

    lesson_7: 函数&函数返回

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,8 @@
-use std::io;
+
+fn five() -> i32 {5}  // 里面的5没有加分号
 
 fn main() {
-    let a = [1, 2, 3, 4, 5];
+    let x = five();
 
-    println!("Please enter an array index.");
-
-    let mut index = String::new();
-
-    io::stdin()
-        .read_line(&mut index)
-        .expect("Failed to read line");
-
-    let index: usize = index
-        .trim()
-        .parse()
-        .expect("Index entered was not a number");
-
-    let element = a[index];
-    println!("The value of element at index {} is {}", index, element);
+    println!("The value of x is {}",x);
 }
