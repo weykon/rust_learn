3b041839365e502cd9d5204a25e2e0aa22096491
weykon <635883092@qq.com>
2022-05-26 15:23:56 +0800

    第二课

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod config;
+mod routes;
 fn main() {
     config::print_config();
+    routes::print_health_route();
     println!("main");
 }
--- src/readme.md
+++ src/readme.md
@@ -2,3 +2,6 @@
 -> mod关键字将其声明一个子模块
 如果 mod my_module;
 那么编译器会去 my_module.rs 或者 my_module/mod.rs 里面找
+
+## 第二课
+用mod.rs
