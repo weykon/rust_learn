0417e2f3597b0c165cc5e91803c7c1c3efb8062a
weykon <635883092@qq.com>
2022-06-04 19:44:45 +0800

    参数约束自定义对于特征上的实现

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,33 @@
-use std::convert::TryInto;
+use std::ops::Add;
 
-fn main() {
-    let a: i32 = 10;
-    let b: u16 = 100;
+#[derive(Debug)]
+struct Point<T: Add<T, Output = T>> {
+    x: T,
+    y: T,
+}
+impl<T: Add<T, Output = T>> Add for Point<T> {
+    type Output = Point<T>;
 
-    let b_ = b.try_into().unwrap();
-
-    if a < b_ { 
-        println!("Ten is less than one hundred.");
+    fn add(self, p: Point<T>) -> Point<T> {
+        Point {
+            x: self.x + p.x,
+            y: self.y + p.y,
+        }
     }
 }
+
+fn add<T: Add<T, Output = T>>(a: T, b: T) -> T {
+    a + b
+}
+
+fn main() {
+    let p1 = Point{x: 1.1f32, y: 1.1f32};
+    let p2 = Point{x: 2.1f32, y: 2.1f32};
+    println!("{:?}", add(p1, p2));
+
+    let p3 = Point{x: 1i32, y: 1i32};
+    let p4 = Point{x: 2i32, y: 2i32};
+    println!("{:?}", add(p3, p4));
+}
+
+// add方法下，参数约束自定义对于特征上的实现
