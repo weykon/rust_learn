6d60ffd204c26352c37b2810ea7cdc5fc3eea5f1
weykon <635883092@qq.com>
2022-06-04 19:36:51 +0800

    引入特征使用方法

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,12 @@
-use std::fmt::Display;
-struct Pair<T> {
-    x: T,
-    y: T,
-}
+use std::convert::TryInto;
 
-impl<T> Pair<T> {
-    fn new(x: T, y: T) -> Self {
-        Self { x, y }
+fn main() {
+    let a: i32 = 10;
+    let b: u16 = 100;
+
+    let b_ = b.try_into().unwrap();
+
+    if a < b_ { 
+        println!("Ten is less than one hundred.");
     }
 }
-
-impl<T: Display + PartialOrd> Pair<T> {
-    fn cmp_display(&self) {
-        if self.x >= self.y {
-            println!("The largest member is x = {}", self.x);
-        } else {
-            println!("The largest member is y = {}", self.y);
-        }
-    }
-} // 以上这个方法，只有在T的类型满足Display+PartialOrd的类型下才能使用cmp_display的方法，
-  // 这个特性就十分吸引我了，
-  //因为在ts里一直以来我都想拥有这个能自适应下的类型配对的书写过程实现
-
-fn main() {}
