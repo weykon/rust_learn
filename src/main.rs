7f3510afe9735dda6329d3a6aa37db95c491ba63
weykon <635883092@qq.com>
2022-06-04 18:04:11 +0800

    trait 的默认方法

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,8 @@
 pub trait Summary {
     fn summarize(&self) -> String;
+    fn defalut_impl(&self) -> String {
+        String::from("(Read more...)")
+    }
 }
 
 pub struct Post {
@@ -23,12 +26,22 @@ impl Summary for Weibo {
     fn summarize(&self) -> String {
         format!("{}发表了微博{}", self.username, self.content)
     }
+    fn defalut_impl(&self) -> String {
+        format!("{}这是默认实现的重载方法咯,", self.username);
+    }
 }
 
 fn main() {
-     let post= Post{ title: "Rust语言简洁".to_string(), author:"SUnface".to_string(),content: "Rust棒极了！".to_string()};
-     let weibo = Weibo{username: "sunface".to_string(),content:"好像微博没有Tweet好用".to_string()};
+    let post = Post {
+        title: "Rust语言简洁".to_string(),
+        author: "SUnface".to_string(),
+        content: "Rust棒极了！".to_string(),
+    };
+    let weibo = Weibo {
+        username: "sunface".to_string(),
+        content: "好像微博没有Tweet好用".to_string(),
+    };
 
-     println!("{}",post.summarize());
-     println!("{}",weibo.summarize());
+    println!("{}", post.summarize());
+    println!("{}", weibo.summarize());
 }
