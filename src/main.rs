ee0c064bc8fb2bda5b999a57271a76f61899d41c
weykon <635883092@qq.com>
2022-06-04 19:48:40 +0800

    对于自定义类型的打印输出

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,14 @@
-use std::ops::Add;
-
 #[derive(Debug)]
-struct Point<T: Add<T, Output = T>> {
-    x: T,
-    y: T,
-}
-impl<T: Add<T, Output = T>> Add for Point<T> {
-    type Output = Point<T>;
-
-    fn add(self, p: Point<T>) -> Point<T> {
-        Point {
-            x: self.x + p.x,
-            y: self.y + p.y,
-        }
-    }
-}
-
-fn add<T: Add<T, Output = T>>(a: T, b: T) -> T {
-    a + b
+struct Point {
+    x: i32,
+    y: i32,
 }
 
 fn main() {
-    let p1 = Point{x: 1.1f32, y: 1.1f32};
-    let p2 = Point{x: 2.1f32, y: 2.1f32};
-    println!("{:?}", add(p1, p2));
-
-    let p3 = Point{x: 1i32, y: 1i32};
-    let p4 = Point{x: 2i32, y: 2i32};
-    println!("{:?}", add(p3, p4));
+    let p = Point { x: 3, y: 3 };
+    println!("{:?}", p);
 }
+// 这里需要对自定义的类型进行标注，才可实现打印输出的功能。
 
-// add方法下，参数约束自定义对于特征上的实现
+// 若想让更自定义的输出，可以使用  std::fmt::Display 特征
+
