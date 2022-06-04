36d3b3a98adfcef5fac71e68a316473912d6d266
weykon <635883092@qq.com>
2022-06-04 18:24:24 +0800

    多重约束和where

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+use std::fmt::Debug;
+use std::fmt::Display;
+
 pub trait Summary {
     fn summarize(&self) -> String;
     fn defalut_impl(&self) -> String {
@@ -5,43 +8,25 @@ pub trait Summary {
     }
 }
 
-pub struct Post {
-    pub title: String,
-    pub author: String,
-    pub content: String,
+pub fn notify(item: &impl Summary) {
+    println!("Breaking news! {}", item.summarize());
+}
+pub fn notifyTwo(item: &impl Summary, item2: &impl Summary) {}
+pub fn notifyPureInGeneric<T: Summary>(item: &T) {}
+pub fn notifyWithGenericForTwoSameT<T: Summary>(item: &T, item2: &T) {}
+
+// 多重约束
+pub fn notifyMutil(item: &(impl Summary + Display)) {}
+pub fn notifyMutilT<T: Summary + Display>(item: &T) {}
+
+// Where
+fn some_function<T: Display + Clone, U: Clone + Debug>(t: &T, u: &U) {}
+// change
+fn some_function_where<T, U>(t: &T, u: &U)
+where
+    T: Display + Clone,
+    U: Clone + Debug,
+{
 }
 
-impl Summary for Post {
-    fn summarize(&self) -> String {
-        format!("文章{},作者是{}", self.title, self.author)
-    }
-}
-
-pub struct Weibo {
-    pub username: String,
-    pub content: String,
-}
-
-impl Summary for Weibo {
-    fn summarize(&self) -> String {
-        format!("{}发表了微博{}", self.username, self.content)
-    }
-    fn defalut_impl(&self) -> String {
-        format!("{}这是默认实现的重载方法咯,", self.username);
-    }
-}
-
-fn main() {
-    let post = Post {
-        title: "Rust语言简洁".to_string(),
-        author: "SUnface".to_string(),
-        content: "Rust棒极了！".to_string(),
-    };
-    let weibo = Weibo {
-        username: "sunface".to_string(),
-        content: "好像微博没有Tweet好用".to_string(),
-    };
-
-    println!("{}", post.summarize());
-    println!("{}", weibo.summarize());
-}
+fn main() {}
