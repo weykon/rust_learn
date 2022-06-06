ee3ba88dd58b2734297d2ecbf73d09453f350999
weykon <635883092@qq.com>
2022-06-06 09:26:42 +0800

    动态指针box<>

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,51 @@
-// 特征对象
-
-pub trait Draw {
-    fn draw(&self);
+trait Draw {
+    fn draw(&self) -> String;
 }
 
-pub struct Button {
-    pub width: u32,
-    pub height: u32,
-    pub label: String,
+impl Draw for u8 {
+    fn draw(&self) -> String {
+        format!("u8: {}", *self)
+    }
 }
 
-impl Draw for Button {
-    fn draw(&self) {}
+impl Draw for f64 {
+    fn draw(&self) -> String {
+        format!("f64: {}", *self)
+    }
 }
 
-struct SelectBox {
-    width: u32,
-    height: u32,
-    options: Vec<String>,
+// 若 T 实现了 Draw 特征， 则调用该函数时传入的 Box<T> 可以被隐式转换成函数参数签名中的 Box<dyn Draw>
+fn draw1(x: Box<dyn Draw>) {
+    // 由于实现了 Deref 特征，Box 智能指针会自动解引用为它所包裹的值，然后调用该值对应的类型上定义的 `draw` 方法
+    x.draw();
 }
 
-impl Draw for SelectBox {
-    fn draw(&self) {}
+fn draw2(x: &dyn Draw) {
+    x.draw();
 }
 
 pub struct Screen {
     pub components: Vec<Box<dyn Draw>>,
 }
 
-fn main() {}
+impl Screen {
+    pub fn run(&self) {
+        for component in self.components.iter() {
+            component.draw();
+        }
+    }
+}
+
+fn main() {
+    let x = 1.1f64;
+    // do_something(&x);
+    let y = 8u8;
+
+    // x 和 y 的类型 T 都实现了 `Draw` 特征，因为 Box<T> 可以在函数调用时隐式地被转换为特征对象 Box<dyn Draw>
+    // 基于 x 的值创建一个 Box<f64> 类型的智能指针，指针指向的数据被放置在了堆上
+    draw1(Box::new(x));
+    // 基于 y 的值创建一个 Box<u8> 类型的智能指针
+    draw1(Box::new(y));
+    draw2(&x);
+    draw2(&y);
+}
