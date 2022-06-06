863f80ca1220ec95841e1ce8ef729a123a6b036f
weykon <635883092@qq.com>
2022-06-06 09:27:20 +0800

    动态指针box<>没明白

--- src/main.rs
+++ src/main.rs
@@ -35,17 +35,49 @@ impl Screen {
         }
     }
 }
+pub struct Button {
+    pub width: u32,
+    pub height: u32,
+    pub label: String,
+}
+
+impl Draw for Button {
+    fn draw(&self) {
+        // 绘制按钮的代码
+    }
+}
+
+struct SelectBox {
+    width: u32,
+    height: u32,
+    options: Vec<String>,
+}
+
+impl Draw for SelectBox {
+    fn draw(&self) {
+        // 绘制SelectBox的代码
+    }
+}
 
 fn main() {
-    let x = 1.1f64;
-    // do_something(&x);
-    let y = 8u8;
+    let screen = Screen {
+        components: vec![
+            Box::new(SelectBox {
+                width: 75,
+                height: 10,
+                options: vec![
+                    String::from("Yes"),
+                    String::from("Maybe"),
+                    String::from("No")
+                ],
+            }),
+            Box::new(Button {
+                width: 50,
+                height: 10,
+                label: String::from("OK"),
+            }),
+        ],
+    };
 
-    // x 和 y 的类型 T 都实现了 `Draw` 特征，因为 Box<T> 可以在函数调用时隐式地被转换为特征对象 Box<dyn Draw>
-    // 基于 x 的值创建一个 Box<f64> 类型的智能指针，指针指向的数据被放置在了堆上
-    draw1(Box::new(x));
-    // 基于 y 的值创建一个 Box<u8> 类型的智能指针
-    draw1(Box::new(y));
-    draw2(&x);
-    draw2(&y);
+    screen.run();
 }
