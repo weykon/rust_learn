2885ce811c87af11238fcefb7bd72a1a61a9ff9e
weykon <635883092@qq.com>
2022-06-17 14:54:54 +0800

    生命周期引子

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,32 @@
-mod config;
-mod routes;
-mod models;
 fn main() {
-    config::print_config();
-    routes::health_route::print_health_route();
-    models::user_model::print_user_model();
-    println!("main");
+    println!("main starting...");
+
+    // 1 -
+    // let r;
+
+    // {
+    //     let x = 5;
+    //     r = &x ;        // here err !
+    // }
+
+    // println!("r: {}", r);
+    // ⬆️ 悬垂指针了
+
+    // 2 -    
+    // 函数中的生命周期
+    // let string1 = String::from("abcd");
+    // let string2 = "xyz";
+    // let result = longest(string1.as_str(), string2);
+    // - - - - - - - - - - - - - - - - -
 }
+
+// 2    - 
+// fn longest(x: &str, y: &str) -> &str {    // 在返回的这个&str中，因为目前不知道是返回x还是y的长度，所以传入的生命周期是未知的
+//     if x.len() > y.len() {
+//         x
+//     } else {
+//         y
+//     }
+// }
+// 这个时候编译器无法自动推导出生命周期，此时需要我们手动去标注
+// - - - - - - - -  
