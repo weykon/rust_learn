bed126c2574ccca08a5ae475b85c28b1ae1fa9c7
weykon <635883092@qq.com>
2022-08-11 13:41:45 +0800

    for mut deref

--- src/main.rs
+++ src/main.rs
@@ -22,7 +22,8 @@ fn main() {
     let x = 5;
     let y = MyCrazyBox::new(x);
     assert_eq!(5, x);
-    // assert_eq!(5, *y);   // 这里 *y 报错，因为不知道如何应对 对于MyCrazyBox 的解引用
+    assert_eq!(5, *y);   // 这里 *y 报错，因为不知道如何应对 对于MyCrazyBox 的解引用
+    // (写完 Deref for MyCrazyBox 就不报错了)
     // 为了启用 * 运算符的解引用功能，需要实现 Deref trait。
     // Used for immutable dereferencing operations, like *v.
     // 用于不可变的解引用操作，例如*v.
@@ -30,3 +31,9 @@ fn main() {
 
 // 当输入 *y 时，Rust 事实上在底层运行了如下代码：
 // *(y.deref())
+
+// 上面那个是应对不可变的类型
+// 以后应对mut的
+// 当 T: Deref<Target=U> 时从 &T 到 &U。
+// 当 T: DerefMut<Target=U> 时从 &mut T 到 &mut U。
+// 当 T: Deref<Target=U> 时从 &mut T 到 &U。
