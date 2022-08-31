701559b49677faf9b80a33bd23e462b3c5135362
weykon <635883092@qq.com>
2022-09-01 03:20:48 +0800

    workspace

---
+++ code2/lib.rs
@@ -0,0 +1,3 @@
+pub fn any_func(){
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,2 @@
-#[cfg(target_os = "linux")]
-fn are_you_on_linux(){
-    println!("You are!");
-}
 
-#[cfg(not(target_os = "linux"))]
-fn are_you_on_linux(){
-    println!("You Not!");
-}
-
-fn main(){
-
-}
+fn main() {}
