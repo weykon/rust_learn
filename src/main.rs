90270d6ed1a12ee864132b850a2b7072127f14e6
weykon <635883092@qq.com>
2022-10-02 17:23:42 +0800

    questionMark

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,29 @@
-use std::fs::File;
-use std::io::prelude::*;
-use std::io::{self, SeekFrom};
-use std::io::Seek;
+struct MyCustomError {
+    code: i32,
+    msg: String,
+}
 
-fn write_file<W: Write + Seek>(writer: &mut W) -> io::Result<()> {
-    writer.seek(SeekFrom::End(-10))?;
+use std::fmt::{Debug, Display, Formatter};
+impl std::error::Error for MyCustomError {}
 
-    for i in 0..10 {
-        writer.write(&[i])?;
+impl Debug for MyCustomError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[{}]{}", self.code, self.msg)
     }
+}
+
+impl Display for MyCustomError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "[{}],{}", self.code, self.msg)
+    }
+}
+
+fn main() -> Result<(), MyCustomError> {
+    let one_error: Result<(), MyCustomError> = Err(MyCustomError {
+        code: -1,
+        msg: "error".to_owned(),
+    });
+    one_error?;
 
     Ok(())
-}
-
-fn example() {
-    let mut file = File::create("foo.md");
-
-    write_file(&mut file)?;
-}
-
-
-
-#[test]
-fn test_writes_bytes(){
-    use std::io::Cursor;
-    let mut buff = Cursor::new(vec![0;15]);
-
-    write_file(&mut buff).unwrap();
-
-    assert_eq!(&buff.get_ref()[5..15], &[0,1,2,3,4,5,6,7,8,9]);
-}
-
-fn main(){
-    
 }
--- src/readme.md
+++ src/readme.md
@@ -1,3 +1,3 @@
-```rust
-use std::io::Cursor
-```
+* Rust编程中主要通过Option<T>和Result<T, E>来传播和处理各种错误。
+
+
