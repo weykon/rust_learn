981b47a19c80f72ba21a790f2d66dd56b5a16d2e
weykon <635883092@qq.com>
2022-10-02 19:06:38 +0800

    lib test

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,3 @@
-use std::{fs::File, io::Read};
-
-
-fn main() -> std::io::Result<()> {
-    let mut open_file = File::open(
-        "./src/Javascript - Objects - Writable, Configurable & Enumerable.md",
-    )?;
-    let mut read_post_string = String::new();
-    open_file.read_to_string(&mut read_post_string);
-    println!("{}", read_post_string);
-    Ok(())
+fn main(){
+    
 }
--- src/readme.md
+++
@@ -1,3 +0,0 @@
-```rust
-use std::io::Cursor
-```
