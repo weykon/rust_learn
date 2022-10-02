82c8f4abc24ad708191f872c4c0ef1cced2756a0
weykon <635883092@qq.com>
2022-10-02 17:36:07 +0800

    write file

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,10 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::io::{self, SeekFrom};
-use std::io::Seek;
 
-fn write_file<W: Write + Seek>(writer: &mut W) -> io::Result<()> {
-    writer.seek(SeekFrom::End(-10))?;
+fn main() -> std::io::Result<()> {
+    let mut file = File:: create("foo.md")?;
 
-    for i in 0..10 {
-        writer.write(&[i])?;
-    }
+    file.write_all(b"Hello, Markdown file!")?;
 
     Ok(())
-}
-
-fn example() {
-    let mut file = File::create("foo.md");
-
-    write_file(&mut file)?;
-}
-
-
-
-#[test]
-fn test_writes_bytes(){
-    use std::io::Cursor;
-    let mut buff = Cursor::new(vec![0;15]);
-
-    write_file(&mut buff).unwrap();
-
-    assert_eq!(&buff.get_ref()[5..15], &[0,1,2,3,4,5,6,7,8,9]);
-}
-
-fn main(){
-    
 }
