94324adf50b83db960ed64ebfbe1d15fc8c76072
weykon <635883092@qq.com>
2022-10-02 18:18:43 +0800

    directly read markdown file to string

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,12 @@
-#![allow(unused)]
-fn main() {
-    use std::fs::File;
-    use std::io::prelude::*;
-    use std::io::{self, SeekFrom};
+use std::{fs::File, io::Read};
 
-    // a library function we've written
-    fn write_ten_bytes_at_end<W: Write + Seek>(writer: &mut W) -> io::Result<()> {
-        writer.seek(SeekFrom::End(-10))?;
 
-        for i in 0..10 {
-            writer.write(&[i])?;
-        }
-
-        // all went well
-        Ok(())
-    }
-
-    fn foo() -> io::Result<()> {
-        // Here's some code that uses this library function.
-        //
-        // We might want to use a BufReader here for efficiency, but let's
-        // keep this example focused.
-        let mut file = File::create("foo.txt")?;
-
-        write_ten_bytes_at_end(&mut file)?;
-        Ok(())
-    }
-
-    // now let's write a test
-    #[test]
-    fn test_writes_bytes() {
-        // setting up a real File is much slower than an in-memory buffer,
-        // let's use a cursor instead
-        use std::io::Cursor;
-        let mut buff = Cursor::new(vec![0; 15]);
-
-        write_ten_bytes_at_end(&mut buff).unwrap();
-
-        assert_eq!(&buff.get_ref()[5..15], &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
-    }
+fn main() -> std::io::Result<()> {
+    let mut open_file = File::open(
+        "./src/Javascript - Objects - Writable, Configurable & Enumerable.md",
+    )?;
+    let mut read_post_string = String::new();
+    open_file.read_to_string(&mut read_post_string);
+    println!("{}", read_post_string);
+    Ok(())
 }
