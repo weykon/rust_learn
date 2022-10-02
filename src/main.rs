767d2619b50f2d0b0605f2cc4696b0678d63ae87
weykon <635883092@qq.com>
2022-10-02 17:44:54 +0800

    file write,add string

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,13 @@ use std::fs::File;
 use std::io::prelude::*;
 
 fn main() -> std::io::Result<()> {
-    let mut file = File:: create("foo.md")?;
-
-    file.write_all(b"Hello, Markdown file!")?;
-
+    let mut file = File::open("foo.md")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    println!("[rust read file, here is contents: ]{}", contents);
+    println!("now I change the contents ");
+    let some_new_words = "--(I add some words)";
+    contents.push_str(some_new_words);
+    println!("{}", contents);
     Ok(())
 }
