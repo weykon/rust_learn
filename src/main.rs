78d33ad65dbea9f1c47bda2fcb4f04e929b71cac
weykon <635883092@qq.com>
2022-10-02 18:02:05 +0800

    official example

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,42 @@
-use std::fs::File;
-use std::io::prelude::*;
+#![allow(unused)]
+fn main() {
+    use std::fs::File;
+    use std::io::prelude::*;
+    use std::io::{self, SeekFrom};
 
-fn main() -> std::io::Result<()> {
-    let mut file = File::open("foo.md")?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
-    println!("[rust read file, here is contents: ]{}", contents);
-    println!("now I change the contents ");
-    let some_new_words = "--(I add some words)";
-    contents.push_str(some_new_words);
-    println!("{}", contents);
-    Ok(())
+    // a library function we've written
+    fn write_ten_bytes_at_end<W: Write + Seek>(writer: &mut W) -> io::Result<()> {
+        writer.seek(SeekFrom::End(-10))?;
+
+        for i in 0..10 {
+            writer.write(&[i])?;
+        }
+
+        // all went well
+        Ok(())
+    }
+
+    fn foo() -> io::Result<()> {
+        // Here's some code that uses this library function.
+        //
+        // We might want to use a BufReader here for efficiency, but let's
+        // keep this example focused.
+        let mut file = File::create("foo.txt")?;
+
+        write_ten_bytes_at_end(&mut file)?;
+        Ok(())
+    }
+
+    // now let's write a test
+    #[test]
+    fn test_writes_bytes() {
+        // setting up a real File is much slower than an in-memory buffer,
+        // let's use a cursor instead
+        use std::io::Cursor;
+        let mut buff = Cursor::new(vec![0; 15]);
+
+        write_ten_bytes_at_end(&mut buff).unwrap();
+
+        assert_eq!(&buff.get_ref()[5..15], &[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+    }
 }
