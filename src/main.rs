46809cf3f2807ed40ffc5b3f42da249cda707bd4
weykon <635883092@qq.com>
2022-10-11 15:48:32 +0800

    在同函数中多个生命周期的很好例子，还有clone的使用也描述了借用情况

--- src/main.rs
+++ src/main.rs
@@ -5,11 +5,21 @@ fn main() {}
 
 fn compare<'a, 'b>(s1: &'a str, s2: &'b str) -> &'a str
 where
-    'a: 'b,
+    'b: 'a,
 {
     if s2.len() > s1.len() {
-        s2  // 在这个函数中，有可能会返回 s2, 而 s2 属于 'b 的 ， 现在是标注好 'a : 'b ，意思是， 'a 在 'b 生命周期外层，包括 'b
-    }else{
+        s2 // 在这个函数中，有可能会返回 s2, 而 s2 属于 'b 的 ， 现在是标注好 'a : 'b ，意思是， 'a 在 'b 生命周期外层，包括 'b
+    } else {
         s1
+    }
+}
+
+// example new one 比如在同一个函数中的不同生命周期
+fn myMultiLife() {
+    let a = "1";
+    {
+        let b = "2";
+        compare(b, a);
+        compare(a.clone(), b.clone());
     }
 }
