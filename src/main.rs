96a4e577ba774428ac153c16ba6681b6f2d3474e
weykon <635883092@qq.com>
2022-10-14 11:06:59 +0800

    feat: borrow and ownship

--- src/main.rs
+++ src/main.rs
@@ -1,49 +1,29 @@
-fn main() {
-    let name = format!("fellow RUstaceans");
-    helper(&name);
-    helper(&name);
-    let r = &name;
-    helper(r);
-    helper(r);
+pub fn main() {
+    let mut name = format!( "add");
 
-    test_mut_situation();
+    update(&mut name);
+    println!("{}", name);
 
-    test_use_a_block_siutuation();
-}
+    let r = &mut name;
 
-fn helper(name: &String) {
+    update(r);
+    update(r);
+
     println!("{}", name);
 }
 
-fn test_mut_situation() {
-    let mut name = format!("fellow RUstaceans");
-
-    // 并不是说就不能用可变，具体是，是哪里开始借用，when
-
-    name.push('x'); // 这里还没有借用
-
-    helper(&name);
-
-    let r = &name;
-
-    // name.push('x');    // 这里会报错了， 因为，他是mut的，
-    // 在上面已经声明 r = &name, 这个push操作很可能就会将原来的数据变了
-    // 下面的helper就可能找不到; 所以在调用的顺序中，有这种思维保留着。
-
-    helper(r); // 这里开始调用，
+fn update(name: &mut String) {
+    name.push_str(".");
 }
+ 
 
-fn test_use_a_block_siutuation() {
-    let mut name = format!("fellow RUstaceans");
 
-    name.push('x');
+// here is the comprehend
+// Ownership: control all access, will free when done 
+// Shared reference: many readers, no writers
+// Mutable reference: no other readers, one writer (???)
 
-    {
-        let r = &name;
-        helper(r);
-        helper(r);
-    } // <-- borrow ends here 
-
-    // r  // out of scope
-    name.push('x');
-}
+// probably should clear about: 
+// 1. Create a shared reference to X "read locks" X.
+// 2. Create a mutable reference to X "writes locks" X.
+// two point both locking lasts until reference goes out of scope
