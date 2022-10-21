fd4d8f16e520c8b48f0a5080b1c5516957d1c4d0
weykon <635883092@qq.com>
2022-10-21 15:22:19 +0800

    thread::join the words join mean

--- src/main.rs
+++ src/main.rs
@@ -36,3 +36,38 @@ fn second_phase(){
 
     handle.join().unwrap();  // 停止thread
 } 
+
+
+
+const NTHREADS: u32 = 10;
+
+// This is the `main` thread
+fn look_and_look() {
+    // Make a vector to hold the children which are spawned.
+    let mut children = vec![];
+
+    for i in 0..NTHREADS {
+        // Spin up another thread
+        children.push(thread::spawn(move || {
+            println!("this is thread number {}", i);
+        }));
+    }
+
+    for child in children {
+        // Wait for the thread to finish. Returns a result.
+        let _ = child.join();
+    }
+}
+
+// 总是忘记 join 的这个意思，很奇怪，我要用自己的语言记住一下.
+// 以上开10个线程，保存在children,
+// join 为什么是等待线程结束的意思呢
+
+// on discord, ask question
+// until join, they work separately, when you join them, it becomes one thread, so at this point they're "joined"
+// main  t1
+//   |   |
+//   |  _|
+//   |_/    # join
+//   |
+// perfect !
