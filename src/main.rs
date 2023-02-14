12a28fa944185ad5aceeb09d524c170eb39c1dff
weykon <635883092@qq.com>
2023-02-14 14:04:33 +0800

    强弱之分

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,25 @@
-// 强指针 、 弱指针
+use std::cell::RefCell;
+use std::rc::Rc;
 
-fn main () { 
+pub struct Test {
+    x: i32,
+}
 
+impl Test {
+    pub fn println(&self) {
+        println!("{}", self.x);
+    }
+}
+
+fn main() {
+    let a = Rc::new(RefCell::new(Test { x: 11 }));
+    a.borrow().println();
+    let a1 = a.clone();
+    let a2 = a.clone();
+
+    a1.borrow_mut().x = 12;
+    a2.borrow().println();
+
+    let a3 = a1.borrow_mut();
+    let a4 = a2.borrow();
 }
