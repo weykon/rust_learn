9b6ef0fd5e025c2ee0cb31a75e5884722d50632c
weykon <635883092@qq.com>
2023-08-29 09:50:31 +0800

    drain & chain zip fold

--- src/main.rs
+++ src/main.rs
@@ -38,9 +38,76 @@ mod tests {
     }
 
     #[test]
-    fn what_drain (){
+    fn what_drain() {
         let mut a = "abc".to_owned();
         a.drain(..1);
-        println!("{}",a);
+        println!("{}", a);
+    }
+
+    #[test]
+    fn complex() {
+        let upper = "Hello World".to_owned();
+        let lowwer = "weykon".to_owned();
+        let (b, mut s) = upper
+            .chars()
+            // 翻转 dlroW olleH
+            .rev()
+            // zip可以用，不过要自己去补齐差集。以下是用0的iter来补齐
+            .zip(
+                lowwer.chars().rev()
+                // chain： 我一开始不解为何还需要chain这样来链接，其实就像是concat一样，命令式地去实现链接
+                .chain(std::iter::repeat('0'))
+            )
+            // 所以现在的： dlroW olleH - nokyew00000.
+            // 那么在zip的作用下，他是这样运行的 (d,n) -> (l,o) -> (r,y) -> ... -> (H,0)
+            // 这样在fold的时候就是处理每一个元组了
+            .fold((false, String::new()), |mut acc: (bool, String), (u, l)| {
+                println!("acc: {:?}, u:{:?}, l:{:?}, ", acc,u,l);
+                match (false, u, l) {
+                    (false, '1', '1') => {
+                        acc.0 = true;
+                        acc.1.push('0');
+                        (acc.0, acc.1)
+                    }
+                    (false, '1', '0') => {
+                        acc.0 = false;
+                        acc.1.push('1');
+                        (acc.0, acc.1)
+                    }
+                    (false, '0', '1') => {
+                        acc.0 = false;
+                        acc.1.push('1');
+                        (acc.0, acc.1)
+                    }
+                    (false, '0', '0') => {
+                        acc.0 = false;
+                        acc.1.push('0');
+                        (acc.0, acc.1)
+                    }
+                    (true, '1', '1') => {
+                        acc.0 = true;
+                        acc.1.push('1');
+                        (acc.0, acc.1)
+                    }
+                    (true, '1', '0') => {
+                        acc.0 = true;
+                        acc.1.push('0');
+                        (acc.0, acc.1)
+                    }
+                    (true, '0', '1') => {
+                        acc.0 = true;
+                        acc.1.push('0');
+                        (acc.0, acc.1)
+                    }
+                    (true, '0', '0') => {
+                        acc.0 = false;
+                        acc.1.push('1');
+                        (acc.0, acc.1)
+                    }
+                    _ => panic!(),
+                }
+            });
+            println!("b: {}, s:{}", b, s);
+            assert_eq!(true,false);
     }
 }
