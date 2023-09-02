4cad74232f265cfc82879cbdf269721c686f4623
weykon <635883092@qq.com>
2023-09-02 17:17:07 +0800

    borrow about arr ?

--- src/course.rs
+++ src/course.rs
@@ -1,6 +1,6 @@
 struct Test {
-    a : u32,
-    b : u32
+    a: u32,
+    b: u32,
 }
 
 impl Test {
@@ -12,9 +12,60 @@ impl Test {
     }
 }
 
-
 pub fn main() {
     let mut t = Test { a: 1, b: 2 };
     t.increase();
     println!("a: {}, b: {}", t.a, t.b);
+}
+
+fn borrow_vec() {
+    // 常常在一些 mut vec 的iter下会出现borrow repeat 问题
+    let mut vec = vec![1, 2, 3];
+    // for i in vec.iter_mut() {
+    //     // if vec[0] != 0 {  这里的vec就会报错了，
+    //     //我看到一个方案说将vec.iter_mut改为0..vec.len();
+    //     // 貌似确实是那么回事，如果只是计数的话。
+    //     // }
+    //     *i += 1;
+    // }
+    
+}
+
+fn another() {
+    let mut s = String::from("hello");
+
+    change(&mut s);
+}
+
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
+}
+
+// 此处背诵
+// 可变引用同时只能存在一个
+// 关于所有权的一些规则必须熟记
+// 可变引用与不可变引用不能同时存在
+// NLL - Non-Lexical Lifetimes(NLL)
+// 专门用于找到某个引用在作用域(})结束前就不再被使用的代码位置。
+
+
+// # 多个可变引用
+fn multi_mutable_reference () { 
+    let mut a = String::from("hello");
+    let b = &mut a;
+    let c = &mut a;
+    // println!("{}, {}", b, c); 
+    // 上面这条打印如果去掉注释，在c的那行的 &mut a 就会报错
+    // 大概是说println使用了a的引用，而使用了的话，也就是
+    // 这个作用域中，即被b借用了，所以c就不能再借用了
+    // 所以在我平时的代码中，vec.iter_mut(),的同一作用域下的，for当中，
+    // vec就不能再被借用了，所以就会报错
+}
+
+// # 可变引用和不可变引用不能同时存在
+fn same_time_at_mutable_ref_and_unmutable_ref (){
+    let mut a = String::from("hello");
+    let b = &a;
+    let c = &mut a;
+    // println!("{}, {}", b, c);
 }
