c9fe6e0b08d790f7677135701a413ca388cc8651
weykon <635883092@qq.com>
2023-09-07 10:11:33 +0800

    FP

--- src/main.rs
+++ src/main.rs
@@ -1,78 +1,22 @@
-use std::thread;
-use std::time::Duration;
+// 让我们先主动试试让函数作为参数
+// 这里非常需要注意的是，fn(i32,i32)->i32
+// 我们是在定义函数类型而不是具体的函数，
+// 可能是我在ts的习惯
+// function math(fn : (a,b)=>{}){}； 所以写了参数名字进去
+fn math(op: fn(i32, i32) -> i32, a: i32, b: i32) -> i32{
+    op(a, b)
+}
 
-// fitness scene
-// the "intensity" of customer preference fot fitness
-
-fn generate_workout(intensity: u32, random_number: u32) {
-    let mut expensive_closure = Cacher::new(|intensity| {
-        println!("calculating slowly...");
-        thread::sleep(Duration::from_secs(2));
-        intensity
-    });
-    //
-    // 原本这里有一条固定的计算已经不见了，
-    // 那么是到了下面具体分支才开始计算
-    //
-    if intensity < 25 {
-        println!("Today, do {} pushups!", expensive_closure.value(intensity));
-        println!("Next, do {} situps!", expensive_closure.value(intensity));
-    } else {
-        if random_number == 3 {
-            println!("Take a break today! Remember to stay hydrated!");
-        } else {
-            println!(
-                "Today, run for {} minutes!",
-                expensive_closure.value(intensity)
-            );
-        }
-    }
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+fn product(a: i32, b: i32) -> i32 {
+    a * b
 }
 
 fn main() {
-    let simulated_user_specified_value = 10;
-    let simulated_random_number = 7;
-
-    generate_workout(simulated_user_specified_value, simulated_random_number);
+    let a = 2;
+    let b = 3;
+    math(add, a, b);
 }
 
-struct Cacher<T>
-where
-    T: Fn(u32) -> u32,
-{
-    calculation: T,
-    value: Option<u32>,
-}
-impl<T> Cacher<T>
-where
-    T: Fn(u32) -> u32,
-{
-    fn new(calculation: T) -> Cacher<T> {
-        Cacher {
-            calculation,
-            value: None,
-        }
-    }
-
-    fn value(&mut self, arg: u32) -> u32 {
-        match self.value {
-            Some(v) => v,
-            None => {
-                let v = (self.calculation)(arg);
-                self.value = Some(v);
-                v
-            }
-        }
-    }
-}
-
-// 函数type 即是 Fn([type]) -> [type]
-
-
-// FnOnce 消费从周围作用域捕获的变量，闭包周围的作用域被称为其 环境，environment。为了消费捕获到的变量，闭包必须获取其所有权并在定义闭包时将其移动进闭包。其名称的 Once 部分代表了闭包不能多次获取相同变量的所有权的事实，所以它只能被调用一次。
-// FnMut 获取可变的借用值所以可以改变其环境
-// Fn 从其环境获取不可变的借用值
-
-// 强制闭包获取其使用的环境值的所有权，
-// 可以在参数列表前使用 move 关键字。
-// 这个技巧在将闭包传递给新线程以便将数据移动到新线程中时最为实用。
