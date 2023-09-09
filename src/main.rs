b6cdb322b97c8e4c55fe3421475abfb64a6c268f
weykon <635883092@qq.com>
2023-09-09 23:56:14 +0800

    btree 带排序的map

--- src/main.rs
+++ src/main.rs
@@ -1,113 +1,30 @@
-fn main() {}
+use std::collections::VecDeque;
 
-mod tests {
-    #[test]
-    fn one() {
-        let a = vec![1, 2, 3];
+fn main() {
+    vecDeque_vec_double_ended_queue();
+    binary_tree_map()
+}
 
-        let b = vec![3, 2, 1];
+fn vecDeque_vec_double_ended_queue() {
+    println!("vecDeque(vec_double_ended_queue)");
+    let mut v1 = VecDeque::new();
+    v1.push_back(1);
+    v1.push_back(2);
+    println!("One: {:?}", v1.pop_front());
+    println!("Two: {:?}", v1.pop_front());
+    v1.push_front(3);
+    v1.push_front(4);
+    println!("Three: {:?}", v1.pop_back());
+    println!("Four: {:?}", v1.pop_back());
+}
 
-        for (a, b) in a.iter().zip(b.iter()) {
-            println!("{},{}", a, b);
-        }
-
-        // 1,3
-        // 2,2
-        // 3,1
-    }
-    #[test]
-    fn watch_TWO() {
-        let a = vec![1, 2, 3, 4, 6];
-        let b = vec![3, 2, 1];
-
-        //zip: 他会忽略多出来的
-        for o in a.iter().zip(b.iter()) {
-            println!("{:?}", o);
-        }
-
-        let mut a_iter = a.iter();
-        let mut b_iter = b.iter();
-        while let Some(x) = a_iter.next() {
-            println!("{}", x);
-            if let Some(y) = b_iter.next() {
-                println!("{}", y);
-            } else {
-                break;
-            }
-        }
-    }
-
-    #[test]
-    fn what_drain() {
-        let mut a = "abc".to_owned();
-        a.drain(..1);
-        println!("{}", a);
-    }
-
-    #[test]
-    fn complex() {
-        let upper = "Hello World".to_owned();
-        let lowwer = "weykon".to_owned();
-        let (b, mut s) = upper
-            .chars()
-            // 翻转 dlroW olleH
-            .rev()
-            // zip可以用，不过要自己去补齐差集。以下是用0的iter来补齐
-            .zip(
-                lowwer.chars().rev()
-                // chain： 我一开始不解为何还需要chain这样来链接，其实就像是concat一样，命令式地去实现链接
-                .chain(std::iter::repeat('0'))
-            )
-            // 所以现在的： dlroW olleH - nokyew00000.
-            // 那么在zip的作用下，他是这样运行的 (d,n) -> (l,o) -> (r,y) -> ... -> (H,0)
-            // 这样在fold的时候就是处理每一个元组了
-            .fold((false, String::new()), |mut acc: (bool, String), (u, l)| {
-                println!("acc: {:?}, u:{:?}, l:{:?}, ", acc,u,l);
-                match (false, u, l) {
-                    (false, '1', '1') => {
-                        acc.0 = true;
-                        acc.1.push('0');
-                        (acc.0, acc.1)
-                    }
-                    (false, '1', '0') => {
-                        acc.0 = false;
-                        acc.1.push('1');
-                        (acc.0, acc.1)
-                    }
-                    (false, '0', '1') => {
-                        acc.0 = false;
-                        acc.1.push('1');
-                        (acc.0, acc.1)
-                    }
-                    (false, '0', '0') => {
-                        acc.0 = false;
-                        acc.1.push('0');
-                        (acc.0, acc.1)
-                    }
-                    (true, '1', '1') => {
-                        acc.0 = true;
-                        acc.1.push('1');
-                        (acc.0, acc.1)
-                    }
-                    (true, '1', '0') => {
-                        acc.0 = true;
-                        acc.1.push('0');
-                        (acc.0, acc.1)
-                    }
-                    (true, '0', '1') => {
-                        acc.0 = true;
-                        acc.1.push('0');
-                        (acc.0, acc.1)
-                    }
-                    (true, '0', '0') => {
-                        acc.0 = false;
-                        acc.1.push('1');
-                        (acc.0, acc.1)
-                    }
-                    _ => panic!(),
-                }
-            });
-            println!("b: {}, s:{}", b, s);
-            assert_eq!(true,false);
+fn binary_tree_map () { 
+    println!("binary_tree_map");
+    let mut map = std::collections::BTreeMap::new();
+    map.insert(3, "c");
+    map.insert(2, "b");
+    map.insert(1, "a");
+    for (key, value) in &map {
+        println!("{}: {}", key, value);
     }
 }
